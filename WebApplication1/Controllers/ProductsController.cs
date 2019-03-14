08606b25a60c0369c1b14c9fdeabb77a0418e69a
HugoVP <dev@paleo.host>
2019-03-14 01:00:13 -0500

    Created Products controller

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TableResult()
+        {
+            using (var context = new SalesEntities())
+            {
+                var result = context.Search(string.Empty, true).ToList();
+                return PartialView(result);
+            }
+        }
+    }
+}
